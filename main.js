a9623ea2a34262ceb74111e5d622fe2e29aa0cb8
Jmirez2022 <Jazi.2000@hotmail.com>
2022-10-25 14:26:19 -0500

    cant get a tie

--- main.js
+++ main.js
@@ -17,14 +17,87 @@ const rockPaperScissors = (hand1, hand2) => {
   // Write code here
   // Use the unit test to see what is expected
 
+//   1. User1 input of rock, paper, or scissors.
+// 1. User2 input of rock, paper, or scissors.
+// 1. Compare User1 input to User2 input.
+// 1. If User1 input is 'rock' and User2 input is 'scissors', User1 wins.
+if (hand1 === 'scissors') {
+  if (hand2 === 'paper') {
+    return "Hand one wins!";
+  } else{
+    return "Hand two wins!"
+  }
+}
+// 1. If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+if (hand1 === 'rock') {
+  if (hand2 === 'paper') {
+    return "Hand two wins!";
+  } else{
+    return "Hand one wins!"
+  }
+}
+// 1. If User1 input is 'rock' and User2 input is 'rock', it's a tie.
+if (hand1 === 'rock') {
+  if (hand2 === 'rock') {
+    return "It's a tie!";
+  } 
+}
+// 1. If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+if (hand1 === 'paper') {
+  if (hand2 === 'rock') {
+    return "Hand one wins!";
+  } else{
+    return "Hand two wins!"
+  }
+}
+// 1. If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+if (hand1 === 'paper') {
+  if (hand2 === 'scissors') {
+    return "Hand two wins!";
+  } else{
+    return "Hand one wins!"
+  }
+}
+
+// 1. If User1 input is 'paper' and User2 input is 'paper', it's a tie.
+if (hand1 === 'paper') {
+  if (hand2 === 'paper') {
+    return "It's a tie!";
+  } 
+}
+
+// 1. If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+if (hand1 === 'scissors') {
+  if (hand2 === 'paper') {
+    return "Hand one wins!";
+  } else{
+    return "Hand two wins!"
+  }
+}
+// 1. If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+if (hand1 === 'scissors') {
+  if (hand2 === 'rock') {
+    return "Hand two wins!";
+  } else{
+    return "Hand one wins!"
+  }
+}
+// 1. If User1 input is 'scissors' and User2 input is 'scissors', it's a tie.
+
+if (hand1 === 'scissors') {
+  if (hand2 === 'scissors') {
+    return "It's a tie!";
+  } 
+}
+  
 }
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
 function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
+  rl.question('hand1: ', (answer1) => { 
+    rl.question('hand2: ', (answer2) => { 
       console.log( rockPaperScissors(answer1, answer2) );
       getPrompt();
     });
@@ -49,9 +122,9 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
   });
 } else {
